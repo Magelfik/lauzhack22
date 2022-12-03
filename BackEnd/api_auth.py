ed1fb9effefc30fe4bb4af3fa5c5e7d416b5f803
Michaël El Kharroubi <michael.el-kharroubi@hesge.ch>
2022-12-03 20:30:49 +0100

    ok

--- BackEnd/api_auth.py
+++ BackEnd/api_auth.py
@@ -2,21 +2,43 @@ import json
 import uuid
 
 from requests import get, post
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, WebSocket
+from typing import List
+from expiring_dict import ExpiringDict
+from dataclasses import dataclass
 
 NULL_UUID: str = "00000000000000000000000000000000"
+DEFAULT_CREDENTIAL_VERIF: str = "4HS52UpbxN8rHqCHWgtCSN:2:e-id:1.0"
+DEFAULT_ATTRIBUTES: List[str] = ["birth"]
+cache: ExpiringDict(600)
+
+
+@dataclass
+class AgeVerificationRequest():
+    connection_id: str
+    process_id: str
+    age_verified: bool
+
 
 app = FastAPI(debug=True)
 
 
 @app.get("/verification/request/")
-def get_verif_request():
+async def get_verif_request():
     token = uuid.uuid1()
     res = json.loads(post("localhost:8080/connection/invitation").json())
 
 
-@app.get("/verification/status/")
-def get_verif_status(token: str = Query(default=NULL_UUID, min_length=32, max_length=32)):
+@app.websocket("/ws/verification/{token}/status")
+async def websocket_verification(websocket: WebSocket, token: str = Query(default=NULL_UUID, min_length=32, max_length=32)):
+    await websocket.accept()
+    while True:
 
-    post("http://localhost:8081/verify/process", ...)
-    pass
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+    #cache.get(token, {})
+    payload: dict = {"connectionId": NULL_UUID,
+                     "credentialDefinitionId": DEFAULT_CREDENTIAL_VERIF,
+                     "attributes": DEFAULT_ATTRIBUTES}
+    res = post("http://localhost:8081/verify/process", json=payload)
+    # recover process id
