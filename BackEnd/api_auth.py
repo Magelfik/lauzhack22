91b80976c62bbdf10e55fcf7d64d8ebae4dc0e1f
Michaël El Kharroubi <michael.el-kharroubi@hesge.ch>
2022-12-03 18:59:11 +0100

    Base

--- BackEnd/api_auth.py
+++ BackEnd/api_auth.py
@@ -2,20 +2,19 @@ import json
 import uuid
 
 from requests import get, post
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
+NULL_UUID: str = "00000000000000000000000000000000"
 
 app = FastAPI(debug=True)
 
 
-@app.get("/invitation")
-def get_invitation():
+@app.get("/verification/request/")
+def get_verif_request():
     token = uuid.uuid1()
     res = json.loads(post("localhost:8080/connection/invitation").json())
 
 
-@app.get("/verify/")
-def verify():
+@app.get("/verification/status/")
+def get_verif_status(token: str = Query(default=NULL_UUID, min_length=32, max_length=32)):
     pass
-
-
