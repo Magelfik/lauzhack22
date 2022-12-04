77f7b7ef2b6e9484bccf5d4bb8bcd818a1ac3f55
Théo PIRKL <pirkl.theo@gmail.com>
2022-12-04 02:03:17 +0100

    All done bitch

---
+++ sites/js/vcms_client.js
@@ -0,0 +1,36 @@
+/**
+ * Initiates a discussion between the auth server. The discussion is incomplete
+ * @param claims
+ * @returns {Promise<any>}
+ */
+function init(claims){
+    return fetch('http://localhost:8000/connection', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"claims": claims})
+    }).then(response => response.json());
+}
+
+/**
+ * Initiates the connection between the auth server and the client
+ * @param token The connection token previously acquired by the init function (see below)
+ * @param claims The required claims
+ * @param on_success The on success callback. Will exclusively be called if all claims are present.
+ * @param on_error The on error callback. Will be called otherwise.
+ */
+function init_websocket(token, claims, on_success, on_error){
+    let ws = new WebSocket("ws://localhost:8000/" + token + "/status");
+
+    ws.onmessage = function(event) {
+        let payload = JSON.parse(event.data);
+
+        if (claims.every(claim => payload.hasOwnProperty(claim))){
+            on_success(payload);
+        } else if (payload.hasOwnProperty('error')){
+            on_error();
+        }
+    };
+}
